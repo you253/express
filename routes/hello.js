9a4331a7f88e2d4c675dda1ce4d9190fc4ee831d
you253 <yo12.white@gmail.com>
2024-09-07 20:09:48 +0900

    いろいろ

--- routes/hello.js
+++ routes/hello.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express 2' });
+  res.render('hello', { title: 'Hello Express' });
 });
 
 module.exports = router;
